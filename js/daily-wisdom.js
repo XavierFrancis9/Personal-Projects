708ec649c1176a682343b04d71a4269076cf1723
Xavier Francis <james.x.francis9@gmail.com>
2023-03-03 13:39:23 -0600

    refector button to generate new output with eaach click

--- js/daily-wisdom.js
+++ js/daily-wisdom.js
@@ -1,9 +1,13 @@
 "use strict";
 
 let phrase = ["\"If it ain't on Github, it doesn't exist.\" - Jordy Muniz", "\"Hardwork beats talent, when talent doesn't work hard.\" - Unknown", "\"Speak softly and carry a big stick.\" - Franklin D. Roosevelt", "\"You don't choose a life, you live one.\" - Unknown"];
-let randomIndex = Math.floor(Math.random() * phrase.length);
-let randomElement = phrase[randomIndex];
 
-myButton.addEventListener("click", () => {
-    output.textContent = randomElement;
+let myButton = document.getElementById('myButton');
+
+myButton.addEventListener('click', () => {
+    let randomIndex = Math.floor(Math.random() * phrase.length);
+    let randomElement = phrase[randomIndex];
+    let output = document.getElementById('output');
+
+    output.innerHTML = randomElement;
 });
